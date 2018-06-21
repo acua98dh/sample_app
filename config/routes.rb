c7897856585526e6176079d58003231139b31553
acua98dh <vbnsibc@yahoo.co.jp>
2018-06-21 12:45:53 +0000

    Finish static pages & Install the Bootstrap 4

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
-
-  get 'static_pages/help'
-
+#  devise_for :users
+#  resources :tasks
+  root 'static_pages#home'
+  
+  match '/help',    to: 'static_pages#help',    via: 'get'
+  match '/about',   to: 'static_pages#about',   via: 'get'
+  match '/contact', to: 'static_pages#contact', via: 'get'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
